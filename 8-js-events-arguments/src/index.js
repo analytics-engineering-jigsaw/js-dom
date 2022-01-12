3df55f51cd2019adfe41a70161e68dfa9691a6cd
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-12 18:06:14 -0500

    update src code

--- 8-js-events-arguments/src/index.js
+++ 8-js-events-arguments/src/index.js
@@ -1,15 +1,10 @@
-// let simpsonEls = Array.from(document.querySelectorAll(".simpsons li"));
-
-// simpsonEls.forEach(function(element){
-//     element.addEventListener('click', logEvent)
-// })
-
-// function logEvent(event){
-//     console.log(`${event.target.innerText} was clicked`)
-// }
-
 let simpsonEls = Array.from(document.querySelectorAll(".simpsons li"));
 
 simpsonEls.forEach(function(element){
-    element.addEventListener('click', (event) =>  console.log(`${event.target.innerText} was clicked`))
+    element.addEventListener('click', logEvent)
 })
+
+function logEvent(event){
+    console.log(`${event.target.innerText} was clicked`)
+}
+
