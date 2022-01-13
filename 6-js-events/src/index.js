ac7993bbda4c78f3d8dfefee75c032a6e9bec5ef
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-13 13:17:43 -0500

    update code

--- 6-js-events/src/index.js
+++ 6-js-events/src/index.js
@@ -1,7 +1,21 @@
-const el = document.querySelector("h1");
+// function early_or_late(name){
+//     let is_early = name[0] == ('a' || 'b')
+//     debugger;
+//     if (is_early){
+//         return `${name} is early`
+//     }
+//     else {
+//         return `${name} is late`
+//     }
+// }
 
-el.addEventListener("click", logStatement)
-
-function logStatement() {
-    console.log('this was clicked')
+function early_or_late(name){
+    is_late = name[0] >= 'l'
+    
+    if (is_late){
+        return `${name} is late`
+    }
+    else {
+        return `${name} is early`
+    }
 }
