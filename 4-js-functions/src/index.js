0182fb046a72d10928f501037161f29acf553f57
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-13 19:24:04 -0500

    update readings

--- 4-js-functions/src/index.js
+++ 4-js-functions/src/index.js
@@ -2,8 +2,56 @@ function greeting(){
     return 'hello world'
 }
 
-// let presidents = ['washington', 'adams', 'jefferson']
 
-// presidents.forEach(function (president) { 
-//     console.log(`Here is ${president}`)
-// })
+
+function earlyOrLate(name) {
+    let earlyLetters = ['a', 'b', 'c']
+    if (earlyLetters.includes(name[0])) {
+        return `${name} is early`
+    }
+    else {
+        return `${name} is late`
+
+    }
+}
+
+// 'sam is late'
+// 'fred is early'
+
+let musicians = ['prince', 'kanye', 'taylor swift']
+
+musicians.forEach((artist) => {
+    if (artist == 'kanye') {
+        console.log(`${artist} is talented but dramatic`)
+    } else {
+        console.log(`${artist} is talented`)
+    }
+})
+
+let nums = [3, 4, 5]
+
+function doubles(nums) {
+    let doubledNums = nums.map( (num) => 2*num )
+    return doubledNums
+}
+
+function getSimpsonNames(){
+    simpsonLis = document.querySelectorAll('ul.simpsons li')
+    simpsonEls = Array.from(simpsonLis)
+    return simpsonEls.map((simpsonEl) => simpsonEl.innerText)
+}
+
+function getCharactersOf(show) {
+    
+    return document.querySelectorAll(`.${show} li`)
+}
+
+
+let title = document.querySelector('h1')
+
+title.addEventListener('mouseenter', logClick)
+
+function logClick(event){
+    debugger;
+    console.log('updated here')
+}
--- 6-js-events/src/index.js
+++ 6-js-events/src/index.js
@@ -1,21 +1,7 @@
-// function early_or_late(name){
-//     let is_early = name[0] == ('a' || 'b')
-//     debugger;
-//     if (is_early){
-//         return `${name} is early`
-//     }
-//     else {
-//         return `${name} is late`
-//     }
-// }
+let h1 = document.querySelector("h1");
 
-function early_or_late(name){
-    is_late = name[0] >= 'l'
-    
-    if (is_late){
-        return `${name} is late`
-    }
-    else {
-        return `${name} is early`
-    }
+h1.addEventListener("click", logStatement)
+
+function logStatement() {
+    console.log('this was clicked')
 }
